6cdad34d7f118a626c3e470b1a7a964c824c86aa
RapestaNValtech <natalia.rapesta@valtech.com>
2020-03-10 00:05:17 -0300

    string change

--- constants/strings.js
+++ constants/strings.js
@@ -1,6 +1,6 @@
 export const strings = {
     enterNumber:'Enter a number',
     reverseCurrenciesLabel:"Put the input money as output money an vice versa.",
-    reverseButton:"ReverseCurrencies",
+    reverseButton:"Reverse Currencies",
     checkInput:'check the input'
 }
